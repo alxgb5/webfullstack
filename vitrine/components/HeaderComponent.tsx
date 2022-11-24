65ee741ff099a72bcd21d343e782b80f02448257
Antony CASTANER <antony.castaner@ynov.com>
2022-11-24 14:55:26 +0100

    Init header and footer with fonts

---
+++ vitrine/components/HeaderComponent.tsx
@@ -0,0 +1,15 @@
+const HeaderComponent: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="left">
+        <img src='/logo.svg' alt='Ride Logo' />
+      
+        <p>RIDE</p>
+      </div>
+
+      <button>button-my-lib-ui</button>
+    </header>
+  )
+}
+
+export default HeaderComponent;
--- vitrine/pages/_app.tsx
+++ vitrine/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/HeaderComponent.scss'
+import '../styles/FooterComponent.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
