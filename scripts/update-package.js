089e3b5bd2791b03cee389a112e1693fc6965da4
Alexandre BG <alexandre.brungiglio@gmail.com>
2022-11-24 16:58:38 +0100

    improve script

--- scripts/update-package.js
+++ scripts/update-package.js
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as child_process from 'child_process';
 
 const exec = child_process.exec;
-const libPath = path.join('..', 'my-lib-ui');
+const libPath = path.join('my-lib-ui');
 const vitrinePath = path.join('..', 'vitrine');
 
 await exec(`cd ${libPath} && npm run rollup && yalc publish && cd ${vitrinePath} && yalc update && npm i`, function (error) { console.log(error); });
