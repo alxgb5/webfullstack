d0412522f23f79187507b876ea88eee919888576
Enzo Avagliano <enzo.avagliano@ynov.com>
2022-11-25 17:00:13 +0100

    Updated phone validator

--- my-lib-ui/src/components/PhoneField/PhoneField.stories.tsx
+++ my-lib-ui/src/components/PhoneField/PhoneField.stories.tsx
@@ -7,8 +7,6 @@ export default {
     component: PhoneField,
 } as ComponentMeta<typeof PhoneField>;
 
-let frenchPhoneRegex = '^((\+)33|0)[1-9](\d{2}){4}$';
-
 
 export const PhoneFieldUI: ComponentStory<typeof PhoneField> = () => {
     const [value, setValue] = useState("");
@@ -19,7 +17,6 @@ export const PhoneFieldUI: ComponentStory<typeof PhoneField> = () => {
                 placeholder='Entrez un numéro'
                 value={value}
                 onChange={(e) => { setValue(e.target.value); }}
-                validator={frenchPhoneRegex}
             />
         </div>);
 };
--- my-lib-ui/src/components/PhoneField/PhoneField.tsx
+++ my-lib-ui/src/components/PhoneField/PhoneField.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './phoneField.css';
 
 export interface PhoneFieldProps {
     label: string;
     placeholder?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     value?: string;
     validator?: string;
 }
 
 const PhoneField = (props: PhoneFieldProps) => {
+
+    const [showError, setShowError] = useState<boolean>(false);
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>, callback: void) {
+        setTimeout(() => {
+            if (/^((\+)33|0)[1-9](\d{2}){4}$/i.test(e.target.value)) {
+                callback;
+            } else {
+                setShowError(true);
+            }
+        }, 1500);
+    }
+
     return (
         <div className='input-field-container'>
             <p>{props.label}</p>
-            <input onChange={props.onChange} value={props.value} type={'tel'} placeholder={props.placeholder || ''} pattern={props.validator} />
+            <input onChange={(e) => handleChange(e, props.onChange(e))} value={props.value} type={'tel'} placeholder={props.placeholder || ''} />
             <div className='line'></div>
         </div>
     );
