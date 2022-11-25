d6fc7c1eb9e1aba7e32bdd646457109113995c76
Antony CASTANER <antony.castaner@ynov.com>
2022-11-25 16:40:57 +0100

    Init register form with email, text and select inputs

--- vitrine/pages/index.tsx
+++ vitrine/pages/index.tsx
@@ -3,6 +3,7 @@ import HeaderComponent from '../components/HeaderComponent'
 import BannerComponent from '../components/BannerComponent'
 import CardComponent from '../components/CardComponent'
 import FooterComponent from '../components/FooterComponent'
+import { InputField, UIButton, EmailField, SelectField } from 'my-lib-ui'
 
 export default function Home() {
   return (
@@ -14,7 +15,35 @@ export default function Home() {
       <main>
         <BannerComponent />
         <CardComponent title={"Inscription"}>
-          <p>TODO register form</p>
+          <form className='register'>
+            <div className='row'>
+              <div className='group'>
+                <InputField label='Nom' />
+              </div>
+              <div className='group'>
+                <InputField label='Prénom' />
+              </div>
+            </div>
+
+            <div className='row'>
+              <div className='group'>
+                <EmailField label='E-mail' />
+              </div>
+              <div className='group'>
+                <EmailField label='E-mail' />
+              </div>
+            </div>
+
+            <div className='row'>
+              <div className='group'>
+                <SelectField label='Nationalité' placeholder='Sélectionner une valeur' />
+              </div>
+            </div>
+
+            <div className='row-right'>
+              <UIButton label='Demander mon inscription' color='primary' />
+            </div>
+          </form>
         </CardComponent>
       </main>
 
