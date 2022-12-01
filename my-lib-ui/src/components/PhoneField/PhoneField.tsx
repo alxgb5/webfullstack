0f11c270f5b96dca5d4f778208f3b10faa924aca
Enzo Avagliano <enzo.avagliano@ynov.com>
2022-12-01 10:02:21 +0100

    Updated phone input

--- my-lib-ui/src/components/PhoneField/PhoneField.tsx
+++ my-lib-ui/src/components/PhoneField/PhoneField.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './phoneField.css';
+import '../Input/input.css'
 
 export interface PhoneFieldProps {
     label: string;
@@ -12,21 +13,30 @@ export interface PhoneFieldProps {
 const PhoneField = (props: PhoneFieldProps) => {
 
     const [showError, setShowError] = useState<boolean>(false);
+    const [valid, setValid] = useState<boolean>(false);
     function handleChange(e: React.ChangeEvent<HTMLInputElement>, callback: void) {
+        console.log('handleChange', e.target.value);
         setTimeout(() => {
             if (/^((\+)33|0)[1-9](\d{2}){4}$/i.test(e.target.value)) {
                 callback;
+                setValid(true);
+                setShowError(false);
             } else {
+                setValid(false);
                 setShowError(true);
             }
-        }, 1500);
+        }, 1000);
     }
 
     return (
-        <div className='input-field-container'>
-            <p>{props.label}</p>
-            <input onChange={(e) => handleChange(e, props.onChange(e))} value={props.value} type={'tel'} placeholder={props.placeholder || ''} />
-            <div className='line'></div>
+        <div>
+            <div className='input-field-container'>
+                <p>{props.label}</p>
+                <input onChange={(e) => handleChange(e, props.onChange(e))} value={props.value} type='tel' placeholder={props.placeholder || ''} />
+                <div className='line' style={{ backgroundColor: 'red' }}></div>
+
+            </div>
+            {showError && <p className='error'>Numéro de téléphone non valide</p>}
         </div>
     );
 };
