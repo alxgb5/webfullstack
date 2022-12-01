0d10b27701d163c84cdef62721c2a914de74778d
Alexandre BG <alexandre.brungiglio@gmail.com>
2022-12-01 10:08:52 +0100

    Passwordfield

---
+++ my-lib-ui/src/components/PasswordField/PasswordField.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import '../Input/input.css';
+
+export interface PasswordFieldProps {
+    label: string;
+    placeholder?: string;
+
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+    value: string;
+}
+
+const PasswordField = (props: PasswordFieldProps) => {
+    const [showError, setShowError] = useState<boolean>(false);
+
+    return (
+        <div>
+            <div className='input-field-container'>
+                <p>{props.label}</p>
+                <input name={'password'} type={'password'} placeholder={props.placeholder || ''} value={props.value} onChange={(e) => { return e; }} />
+                <div className='line'></div>
+            </div>
+            {
+                showError ? <p className='error'>Le format d'email est incorrect.</p> : <></>
+            }
+        </div>
+    );
+};
+
+export default PasswordField;
