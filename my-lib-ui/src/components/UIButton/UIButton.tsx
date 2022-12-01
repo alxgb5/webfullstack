8c04c8c2ed46181fdff7c73e5e63647518512d02
Alexandre BG <alexandre.brungiglio@gmail.com>
2022-12-01 14:12:44 +0100

    props height and width

--- my-lib-ui/src/components/UIButton/UIButton.tsx
+++ my-lib-ui/src/components/UIButton/UIButton.tsx
@@ -7,12 +7,14 @@ export interface UIButtonProps {
     color?: 'primary' | 'light' | 'dark';
 
     className?: string;
+    width?: string;
+    height?: string;
 }
 
 const UIButton = (props: UIButtonProps) => {
     if (props.color == null)
         props.color = 'primary';
-    return <button onClick={props.onClick} className={'ui-btn ' + props.color + ' ' + props.className}>{props.label}</button>;
+    return <button onClick={props.onClick} style={{ height: props.height || '', width: props.width || '' }} className={'ui-btn ' + props.color + ' ' + props.className}>{props.label}</button>;
 };
 
 export default UIButton;
