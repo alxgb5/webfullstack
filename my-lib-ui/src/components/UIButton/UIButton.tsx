a6600e584cd626cce4b511ae5719b652bd4d2897
Alexandre BG <alexandre.brungiglio@gmail.com>
2022-12-01 14:03:29 +0100

    classNmae on button

--- my-lib-ui/src/components/UIButton/UIButton.tsx
+++ my-lib-ui/src/components/UIButton/UIButton.tsx
@@ -5,12 +5,14 @@ export interface UIButtonProps {
     label: string;
     onClick?: () => void;
     color?: 'primary' | 'light' | 'dark';
+
+    className?: string;
 }
 
 const UIButton = (props: UIButtonProps) => {
     if (props.color == null)
         props.color = 'primary';
-    return <button onClick={props.onClick} className={'ui-btn ' + props.color}>{props.label}</button>;
+    return <button onClick={props.onClick} className={'ui-btn ' + props.color + ' ' + props.className}>{props.label}</button>;
 };
 
 export default UIButton;
