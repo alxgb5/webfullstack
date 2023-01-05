e869365258d17177b245d18b8b18b7049f51fded
Antony CASTANER <antony.castaner@ynov.com>
2023-01-05 16:22:08 +0100

    Use validator to check registration

--- user/src/Controller/InscriptionApiController.php
+++ user/src/Controller/InscriptionApiController.php
@@ -9,35 +9,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/inscription')]
 class InscriptionApiController extends AbstractController
 {
     #[Route('/', methods: 'POST')]
-    public function inscription(Request $request, ManagerRegistry $doctrine): Response
+    public function inscription(Request $request, ManagerRegistry $doctrine, SerializerInterface $serializer, ValidatorInterface $validator): Response
     {
-        $data = json_decode($request->getContent(), true);
+        $futureUser = $serializer->deserialize($request->getContent(), FutureUser::class, 'json');
 
-        if (!isset($data, $data['firstname'], $data['lastname'], $data['phone_number'], $data['email'], $data['nationality'])) {
-            return $this->json('Invalid body: missing fields');
+        $errors = $validator->validate($futureUser);
+
+        if ($errors->count() > 0) {
+            return $this->json($errors);
         }
 
         $entityManager = $doctrine->getManager();
 
-        $futureUserWithSameEmail = $entityManager
-            ->getRepository(FutureUser::class)
-            ->findOneBy(['email' => $data['email']]);
-
-        if (isset($futureUserWithSameEmail)) {
-            return $this->json('Email already used');
-        }
-
-        $futureUser = new FutureUser();
-        $futureUser->setFirstname($data['firstname']);
-        $futureUser->setLastname($data['lastname']);
-        $futureUser->setEmail($data['email']);
-        $futureUser->setPhoneNumber($data['phone_number']);
-        $futureUser->setNationality($data['nationality']);
         $futureUser->setValidated(false);
 
         $entityManager->persist($futureUser);
--- user/src/Entity/FutureUser.php
+++ user/src/Entity/FutureUser.php
@@ -4,31 +4,43 @@ namespace App\Entity;
 
 use App\Repository\FutureUserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: FutureUserRepository::class)]
+#[UniqueEntity('email')]
 class FutureUser
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Assert\IsNull()]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(max: 50)]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(max: 50)]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 10)]
+    #[Assert\NotBlank()]
     private ?string $phone_number = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
     private ?string $nationality = null;
 
     #[ORM\Column]
+    #[Assert\IsNull()]
     private ?bool $validated = null;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Assert\Email()]
     private ?string $email = null;
 
     public function getId(): ?int
