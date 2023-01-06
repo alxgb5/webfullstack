5c73bf727d1dac1605039b07ed8764caeb6e57a7
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-01-06 13:53:17 +0100

    login page

--- vitrine/pages/login.tsx
+++ vitrine/pages/login.tsx
@@ -3,8 +3,31 @@ import HeaderComponent from '../components/HeaderComponent';
 import FooterComponent from '../components/FooterComponent';
 import Link from 'next/link';
 import { InputField, PasswordField, UIButton } from 'my-lib-ui';
+import { FormEvent, MouseEvent, useState } from 'react';
 
 export default function Login() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    e.preventDefault();
+
+    const user = {
+      username: username,
+      password: password,
+    };
+    fetch('http://localhost:8000/api/.user/login', { body: JSON.stringify(user), method: 'POST' }).then((response) => {
+      console.log("🚀 ~ fetch ~ response", response);
+      setLoading(false);
+    }, (error) => {
+      console.log("🚀 ~ fetch ~ error", error);
+      setLoading(false);
+
+    });
+  };
+
   return (
     <div>
       <HeadComponent />
@@ -18,10 +41,10 @@ export default function Login() {
           <div className='loginForm'>
             <h1>Connexion</h1>
 
-            <form>
-              <InputField label='identifiant' />
-              <PasswordField label='mot de passe' onChange={(e) => { }} value={''} />
-              <UIButton label='Connexion' color='primary' />
+            <form onSubmit={(e) => { handleSubmit(e); }}>
+              <InputField label='identifiant' value={username} onChange={(e) => { setUsername(e.target.value); }} />
+              <PasswordField label='mot de passe' value={password} onChange={(e) => { setPassword(e.target.value); }} />
+              <UIButton label='Connexion' color='primary' type={'submit'} disabled={loading} />
             </form>
           </div>
         </div>
