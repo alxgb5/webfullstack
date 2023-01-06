1a0bc7de52dbc64b1bad3eaf9000e5d258698ecb
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-01-06 12:02:18 +0100

    disabled

--- my-lib-ui/src/components/UIButton/UIButton.tsx
+++ my-lib-ui/src/components/UIButton/UIButton.tsx
@@ -9,12 +9,13 @@ export interface UIButtonProps {
     width?: string;
     height?: string;
     type: "button" | "reset" | "submit" | undefined;
+    disabled: boolean;
 }
 
 const UIButton = (props: UIButtonProps) => {
     if (props.color == null)
         props.color = 'primary';
-    return <button onClick={(e) => { props.onClick ? props.onClick(e) : null; }} style={{ height: props.height || '', width: props.width || '' }} className={'ui-btn ' + props.color + ' ' + props.className} type={props.type}>{props.label}</button>;
+    return <button onClick={(e) => { props.onClick ? props.onClick(e) : null; }} style={{ height: props.height || '', width: props.width || '' }} className={'ui-btn ' + props.color + ' ' + props.className} type={props.type} disabled={props.disabled}>{props.label}</button>;
 };
 
 export default UIButton;
