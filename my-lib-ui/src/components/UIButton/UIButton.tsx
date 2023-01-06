5081e267905177f57222f36272be1412d01a53e9
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-01-06 11:44:51 +0100

    uibutton

--- my-lib-ui/src/components/UIButton/UIButton.tsx
+++ my-lib-ui/src/components/UIButton/UIButton.tsx
@@ -3,17 +3,18 @@ import './UIButton.css';
 
 export interface UIButtonProps {
     label: string;
-    onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     color?: 'primary' | 'light' | 'dark';
     className?: string;
     width?: string;
     height?: string;
+    type: "button" | "reset" | "submit" | undefined;
 }
 
 const UIButton = (props: UIButtonProps) => {
     if (props.color == null)
         props.color = 'primary';
-    return <button onClick={(e) => { props.onClick(e); }} style={{ height: props.height || '', width: props.width || '' }} className={'ui-btn ' + props.color + ' ' + props.className}>{props.label}</button>;
+    return <button onClick={(e) => { props.onClick!(e); }} style={{ height: props.height || '', width: props.width || '' }} className={'ui-btn ' + props.color + ' ' + props.className} type={props.type}>{props.label}</button>;
 };
 
 export default UIButton;
