a4df9ba6b74d56bdf296f264b5c4c85a1b49c06f
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-01-19 15:58:50 +0100

    fix script

--- scripts/init-package.js
+++ scripts/init-package.js
@@ -4,9 +4,17 @@ import * as child_process from 'child_process';
 const exec = child_process.exec;
 const libPath = path.join('my-lib-ui');
 const vitrinePath = path.join('..', 'vitrine');
+const electronPath = path.join('..', 'electron');
 
 await exec(`cd ${libPath} && npm i && npm run rollup && yalc publish && cd ${vitrinePath} && yalc add my-lib-ui && npm i && yalc update && npm i`, function (error) {
     if (error)
         console.log(error);
-    else console.log('Le script a été correctement initialisé');
+    else console.log('Le script a été correctement initialisé dans vitrine');
 });
+
+await exec(`cd ${libPath} && npm i && npm run rollup && yalc publish && cd ${electronPath} && yalc add my-lib-ui && npm i && yalc update && npm i`, function (error) {
+    if (error)
+        console.log(error);
+    else console.log('Le script a été correctement initialisé dans electron');
+});
+
--- scripts/update-package.js
+++ scripts/update-package.js
@@ -4,9 +4,16 @@ import * as child_process from 'child_process';
 const exec = child_process.exec;
 const libPath = path.join('my-lib-ui');
 const vitrinePath = path.join('..', 'vitrine');
+const electronPath = path.join('..', 'electron');
 
 await exec(`cd ${libPath} && npm run rollup && yalc publish && cd ${vitrinePath} && yalc update && npm i`, function (error) {
     if (error)
         console.log(error);
-    else console.log('Le script a été correctement initialisé');
+    else console.log('Le script a été correctement mis à jours dans vitrine');
+});
+
+await exec(`cd ${libPath} && npm run rollup && yalc publish && cd ${electronPath} && yalc update && npm i`, function (error) {
+    if (error)
+        console.log(error);
+    else console.log('Le script a été correctement mis à jours dans electron');
 });
